f6eb8c7dabd407d7be9b02484540892696534668
Ale Gomez <gom20347@uvg.edu.gt>
2023-08-04 20:31:07 -0600

    added constants

---
+++ Constants.py
@@ -0,0 +1,10 @@
+PLUGIN_NAMES = [
+    "xep_0030",  # Service Discovery
+    "xep_0199",  # Ping
+    "xep_0045",  # Multi-User Chat (MUC)
+    "xep_0085",  # Chat State Notifications
+    "xep_0004",  # Data Forms
+    "xep_0060",  # Publish-Subscribe (PubSub)
+    "xep_0066",  # Out of Band Data
+    "xep_0363",  # HTTP File Upload
+]
